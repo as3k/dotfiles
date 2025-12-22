f4396fea3e11538e87c912d0e34c88e3031deba3
as3k <zack.grrr@gmail.com>
2025-12-22 11:48:39 -0800

    update keymaps to allow neovim splitting

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -2,3 +2,13 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 vim.api.nvim_set_keymap("i", "jj", "<Esc>", { noremap = false })
+
+-- Smart resize support for tmux + Neovim
+
+-- These are mostly no-ops unless tmux forwards the keys,
+-- but defining them makes behavior explicit and predictable.
+
+vim.keymap.set("n", "<C-w><", "<C-w><", { silent = true })
+vim.keymap.set("n", "<C-w>>", "<C-w>>", { silent = true })
+vim.keymap.set("n", "<C-w>+", "<C-w>+", { silent = true })
+vim.keymap.set("n", "<C-w>-", "<C-w>-", { silent = true })
